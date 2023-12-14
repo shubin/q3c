500e973bc43d20693490cf89f2c6c714233d4399
serge <serge@generalarcade.com>
2023-12-14 20:52:13 +0800

    Formatting

--- mod/code/ui/ui_syscalls.c
+++ mod/code/ui/ui_syscalls.c
@@ -396,31 +396,31 @@ int trap_RealTime(qtime_t *qtime) {
 
 // this returns a handle.  arg0 is the name in the format "idlogo.roq", set arg1 to NULL, alteredstates to qfalse (do not alter gamestate)
 int trap_CIN_PlayCinematic( const char *arg0, int xpos, int ypos, int width, int height, int bits) {
-  return syscall(UI_CIN_PLAYCINEMATIC, arg0, xpos, ypos, width, height, bits);
+  return syscall( UI_CIN_PLAYCINEMATIC, arg0, xpos, ypos, width, height, bits );
 }
  
 // stops playing the cinematic and ends it.  should always return FMV_EOF
 // cinematics must be stopped in reverse order of when they are started
-e_status trap_CIN_StopCinematic(int handle) {
+e_status trap_CIN_StopCinematic( int handle ) {
   return (e_status)syscall(UI_CIN_STOPCINEMATIC, handle);
 }
 
 
 // will run a frame of the cinematic but will not draw it.  Will return FMV_EOF if the end of the cinematic has been reached.
-e_status trap_CIN_RunCinematic (int handle) {
-  return (e_status)syscall(UI_CIN_RUNCINEMATIC, handle);
+e_status trap_CIN_RunCinematic( int handle ) {
+  return (e_status)syscall( UI_CIN_RUNCINEMATIC, handle );
 }
  
 
 // draws the current frame
-void trap_CIN_DrawCinematic (int handle) {
-  syscall(UI_CIN_DRAWCINEMATIC, handle);
+void trap_CIN_DrawCinematic( int handle ) {
+  syscall( UI_CIN_DRAWCINEMATIC, handle );
 }
  
 
 // allows you to resize the animation dynamically
-void trap_CIN_SetExtents (int handle, int x, int y, int w, int h) {
-  syscall(UI_CIN_SETEXTENTS, handle, x, y, w, h);
+void trap_CIN_SetExtents( int handle, int x, int y, int w, int h ) {
+  syscall( UI_CIN_SETEXTENTS, handle, x, y, w, h );
 }
 
 
