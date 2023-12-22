6cfdef2d2604cc1a11d20e123a577e26f5d4f274
serge <serge@generalarcade.com>
2023-12-22 19:07:45 +0800

    Work on Rml integrations

---
+++ prog/code/qc_ui/ui_fileinterface.cpp
@@ -0,0 +1,103 @@
+extern "C" {
+#include "../qcommon/q_shared.h"
+#include "../qcommon/qcommon.h"
+#include "ui_public.h"
+}
+#undef DotProduct
+
+#include <RmlUi/Core/Core.h>
+#include <RmlUi/Core/FileInterface.h>
+
+#include "ui_local.h"
+
+namespace {
+
+struct FileData {
+	fileHandle_t	qfile;
+	int				size;
+	int				pos;
+};
+
+}
+
+using namespace Rml;
+
+QFileInterface::QFileInterface() {
+}
+
+QFileInterface::~QFileInterface() {
+}
+
+FileHandle QFileInterface::Open( const String &path ) {
+	FileData *fd = new FileData();
+
+	fd->size = trap_FS_FOpenFile( path.c_str(), &fd->qfile, FS_READ );
+	if ( fd->qfile == 0 ) {
+		delete fd;
+		return 0;
+	}
+	fd->pos = 0;
+	return (FileHandle)fd;
+}
+
+void QFileInterface::Close( FileHandle file ) {
+	FileData *fd = (FileData*)file;
+	if ( fd != NULL ) {
+		trap_FS_FCloseFile( fd->qfile );
+		delete fd;
+	}
+}
+
+size_t QFileInterface::Read( void *buffer, size_t size, FileHandle file ) {
+	FileData *fd = (FileData*)file;
+
+	if ( fd == NULL ) {
+		trap_Error( "qcui: QFileInterface::Read: wrong file handle" );
+		return 0;
+	}
+
+	if ( fd->pos + size > fd->size ) {
+		size -= fd->pos + size - fd->size;
+	}
+
+	if ( size > 0 ) {
+		trap_FS_Read( buffer, ( int )size, ( fileHandle_t )file );
+		fd->pos += size;
+	}
+	return size;
+}
+
+bool QFileInterface::Seek( FileHandle file, long offset, int origin ) {
+	trap_Error( "qcui: QFileInterface::Seek: not supported" );
+	return false;
+}
+
+size_t QFileInterface::Tell( FileHandle file ) {
+	trap_Error( "qcui: FileInterface::Tell: not supported\n" );
+	return 0;
+}
+
+size_t QFileInterface::Length( FileHandle file ) {
+	FileData *fd = (FileData*)file;
+
+	if ( fd == NULL ) {
+		trap_Error( "qcui: QFileInterface::Read: wrong file handle" );
+		return 0;
+	}
+
+	return fd->size;
+}
+
+bool QFileInterface::LoadFile( const String &path, String &out_data ) {	
+	fileHandle_t qfile;
+	int size;
+
+	size = trap_FS_FOpenFile( path.c_str(), &qfile, FS_READ );
+	if ( qfile == 0 ) {
+		return false;
+	}
+	out_data.resize( size );
+	trap_FS_Read( &out_data[0], size, qfile );
+	trap_FS_FCloseFile( qfile );
+	return true;
+}
---
+++ prog/code/qc_ui/ui_renderinterface.cpp
@@ -0,0 +1,39 @@
+#include "../qcommon/q_shared.h"
+#include "ui_public.h"
+#undef DotProduct
+
+#include <RmlUi/Core/Core.h>
+#include <RmlUi/Core/RenderInterface.h>
+
+#include "ui_local.h"
+
+using namespace Rml;
+
+QRenderInterface::QRenderInterface() {
+}
+
+QRenderInterface::~QRenderInterface() {
+}
+
+void QRenderInterface::RenderGeometry( Vertex *vertices, int num_vertices, int *indices, int num_indices, TextureHandle texture, const Vector2f &translation ) {
+}
+
+void QRenderInterface::EnableScissorRegion( bool enable ) {
+}
+
+void QRenderInterface::SetScissorRegion( int x, int y, int width, int height ) {
+}
+
+bool QRenderInterface::LoadTexture( TextureHandle &texture_handle, Vector2i &texture_dimensions, const String &source ) {
+	return true;
+}
+
+bool QRenderInterface::GenerateTexture( TextureHandle &texture_handle, const byte *source, const Vector2i &source_dimensions ) {
+	return true;
+}
+
+void QRenderInterface::ReleaseTexture( TextureHandle texture ) {
+}
+
+void QRenderInterface::SetTransform( const Matrix4f *transform ) {
+}
