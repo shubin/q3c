4d60663b166c953186be59d600bdeb1ecec4ff2a
serge_shubin <serge@generalarcade.com>
2023-12-25 05:39:27 +0800

    Basic setup: name editing

--- prog/code/qc_ui/shell/menu/setup.lua
+++ prog/code/qc_ui/shell/menu/setup.lua
@@ -2,11 +2,8 @@ menu.setup = {}
 menu.setup.doc = LoadDocument("menu/setup.rml")
 
 function menu.setup:show()
-  trap_Print("Setup show!\n")
   local name_elem = self.doc:GetElementById("param_name");
-  local name = trap_Cvar_VariableString("name")
-  trap_Print("Name: "..name.."\n")
-  name_elem:SetAttribute("value", name)
+  name_elem:SetAttribute("value", trap_Cvar_VariableString("name"))
 end
 
 function menu.setup:back()
@@ -16,9 +13,5 @@ end
 
 function menu.setup:apply()
   local name_elem = self.doc:GetElementById("param_name");
-  if name_elem then
-    trap_Print("Can get name!\n")
-  else
-    trap_Print("Can't get name!\n")
-  end
+  trap_Cvar_Set("name", name_elem:GetAttribute("value"))
 end
